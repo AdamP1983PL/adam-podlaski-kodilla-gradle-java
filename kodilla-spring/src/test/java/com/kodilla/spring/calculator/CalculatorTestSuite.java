c73e48a5324740ed3265f987820611b90d3bcd6e
Adam Podlaski <podlaski83@gmail.com>
2023-09-26 21:49:13 +0200

    spring exercises

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CalculatorTestSuite {
+
+//    @Autowired
+//    Calculator calculator;
+//
+//    @Test
+//    void testCalculations() {
+//        // given
+//
+//        // when
+//        double addResult = calculator.add(5.0, 5.0);
+//        double subResult = calculator.sub(5.0, 5.0);
+//        double mulResult = calculator.mul(5.0, 5.0);
+//        double divResult = calculator.div(5.0, 5.0);
+//
+//        // then
+//        assertEquals(10.0, addResult);
+//        assertEquals(0, subResult);
+//        assertEquals(25.0, mulResult);
+//        assertEquals(1.0, divResult);
+//    }
+}
